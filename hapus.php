824b3f1935dfa221c47da4a5b33592adb837cf7d
nisa_ashfiyani <ashfii.yaa22@gmail.com>
2022-03-28 15:08:40 +0700

    wp2

--- hapus.php
+++ hapus.php
@@ -1,13 +1,19 @@
 <?php
-echo $_GET['nim'];
+//echo $_GET["nim"];
+require_once('koneksi.php');
 
-include('koneksi.php');
 $sql = "delete from mahasiswa where nim='" . $_GET['nim'] . "'";
-$result = pg_affected_rows(pg_query($sql));
-if($result == 1) {
+$result = pg_query($sql);
+
+if($result) {
     echo '<script type="text/javascript">';
     echo 'alert("Data telah terhapus");';
-    echo 'window.location.href="index.php";';
-    echo "</script>";
+    echo 'window.location.href = "index.php";';
+    echo '</script>';
+} else {
+    echo '<script type="text/javascript">';
+    echo 'alert("Data GAGAL terhapus");';
+    echo 'window.location.href = "index.php";';
+    echo '</script>';
 }
 ?>
